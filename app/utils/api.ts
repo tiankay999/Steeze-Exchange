7b4e042e6a3bdcfeeb30bb136c577e5bd0a033c8
tiankay999 <tiankay999@gmail.com>
2025-12-08 16:35:23 +0000

    my backend service updated

--- app/utils/api.ts
+++ app/utils/api.ts
@@ -1,3 +1,5 @@
 
 
-export const API_BASE_URL = "steeze-exchange-backend-production.up.railway.app";
+export const API_BASE_URL = process.env.NODE_ENV === 'production'
+    ? "https://steeze-exchange-backend-1.onrender.com"
+    : "http://localhost:5007";
