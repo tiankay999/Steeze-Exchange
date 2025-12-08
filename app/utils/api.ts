12b63ea3742eeb381613f63c16ff0fbb289c71cc
tiankay999 <tiankay999@gmail.com>
2025-12-08 16:55:23 +0000

    updated the errors in frontend

--- app/utils/api.ts
+++ app/utils/api.ts
@@ -1,5 +1,4 @@
 
 
-export const API_BASE_URL = process.env.NODE_ENV === 'production'
-    ? "https://steeze-exchange-backend-1.onrender.com"
-    : "http://localhost:5007";
+export const API_BASE_URL ="https://steeze-exchange-backend-1.onrender.com"
+   
